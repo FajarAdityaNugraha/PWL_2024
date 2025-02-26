12b65d495621bdfcfef03183b407fb289ab58b75
FajarAdityaNugraha <fajaradityanugraha01@gmail.com>
2025-02-26 07:28:54 +0700

    Jobsheet 2 Latihan Praktikum

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +22,82 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('hello');
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+Route::get('/world', function () { 
+    return 'World';
 });
+
+Route::get('/', function () { 
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () { 
+    return '2341720198 Fajar Aditya Nugraha';
+});
+
+Route::get('/user/{name}', function ($name) { 
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID " . $id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/profile', function() {
+    //
+    })->name('profile');
+
+
+Route::middleware(['first', 'second'])->group(function () { 
+    Route::get('/', function () {
+    // Uses first & second middleware...
+    });
+Route::get('/user/profile', function () {
+    // Uses first & second middleware...
+    });
+});
+        
+Route::domain('{account}.example.com')->group(function () { 
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+Route::middleware('auth')->group(function () { 
+    Route::get('/user', [UserController::class, 'index']); 
+    Route::get('/post', [PostController::class, 'index']); 
+    Route::get('/event', [EventController::class, 'index']);
+});
+
+Route::prefix('admin')->group(function () { 
+    Route::get('/user', [UserController::class, 'index']); 
+    Route::get('/post', [PostController::class, 'index']); 
+    Route::get('/event', [EventController::class, 'index']);
+});
+
+Route::redirect('/here', '/there');
+
+Route::view('/welcome', 'welcome'); Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+Route::get('/', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+Route::get('/', HomeController::class);
+Route::get('/about', AboutController::class);
+Route::get('/articles/{id}', ArticleController::class);
+
+Route::resource('photos', PhotoController::class)->only(['index', 'show'
+]);
+Route::resource('photos', PhotoController::class)->except([ 'create', 'store', 'update', 'destroy'
+]);
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
